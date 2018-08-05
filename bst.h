551df689c24f7f9d3e61fb68bc1103e334a4b1c6
pollockcl <pollock.clee@gmail.com>
2018-08-04 18:32:30 -0600

    added insertion function

--- bst.h
+++ bst.h
@@ -8,3 +8,26 @@ typedef struct Node
     struct Node *right;
 }Node;
 
+Node insert(Node *node, int data)
+{
+	if (node->data == 0)
+	{
+		node->data = data;
+		node->left = malloc(sizeof(Node));
+		node->right = malloc(sizeof(Node));
+	}
+	else if (data > node->data)
+	{
+		insert(node->right, data);
+	}
+	else if (data < node->data)
+	{
+		insert(node->left, data);
+	}
+	else
+	{
+		puts("Node already exists");
+	}
+
+	return *node;
+}
--- main.c
+++ main.c
@@ -5,6 +5,13 @@
 int main(void)
 {
     Node *root = malloc(sizeof(Node));
-    root->data = 10;
+	insert(root, 10);
+	insert(root, 10);
+	insert(root, 5);
+	insert(root, 15);
+	insert(root, 13);
     printf("%d\n", root->data);
+    printf("%d\n", root->left->data);
+    printf("%d\n", root->right->data);
+    printf("%d\n", root->right->left->data);
 }
