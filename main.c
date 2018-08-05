4036f199ea073512578e7104539ce5024cbdc79e
pollockcl <pollock.clee@gmail.com>
2018-08-04 19:31:42 -0600

    just display stuff

--- main.c
+++ main.c
@@ -6,26 +6,30 @@ int main(void)
 {
     Node *root = malloc(sizeof(Node));
     puts("Inserting...");
-	printf("%d\n", insert(root, 40)->data);
-	printf("%d\n", insert(root, 29)->data);
-	printf("%d\n", insert(root, 12)->data);
-	printf("%d\n", insert(root, 89)->data);
-	printf("%d\n", insert(root, 1)->data);
-	printf("%d\n", insert(root, 35)->data);
-	printf("%d\n", insert(root, 20)->data);
-	printf("%d\n", insert(root, 99)->data);
-	printf("%d\n", insert(root, 41)->data);
-	printf("%d\n", insert(root, 34)->data);
-	printf("%d\n", insert(root, 89)->data);
+    printf("%d\n", insert(root, 40)->data);
+    printf("%d\n", insert(root, 29)->data);
+    printf("%d\n", insert(root, 12)->data);
+    printf("%d\n", insert(root, 89)->data);
+    printf("%d\n", insert(root, 1)->data);
+    printf("%d\n", insert(root, 35)->data);
+    printf("%d\n", insert(root, 20)->data);
+    printf("%d\n", insert(root, 99)->data);
+    printf("%d\n", insert(root, 41)->data);
+    printf("%d\n", insert(root, 34)->data);
+    printf("%d\n", insert(root, 89)->data);
 
     puts("Finding...");
-    printf("Not Found %d\n", find(root, 30));
-    printf("Found %d\n", find(root, 40));
-    printf("Found %d\n", find(root, 20));
-    printf("Found %d\n", find(root, 99));
-    printf("Not Found %d\n", find(root, 120));
+    printf("Not Found %d\n", find(root, 30)); // fix display
+    printf("Found %d\n", find(root, 40)->data);
+    printf("Found %d\n", find(root, 20)->data);
+    printf("Found %d\n", find(root, 99)->data);
+    printf("Not Found %d\n", find(root, 120)); //fix display
 
-    puts("Sorting....");
+    puts("Sorting...");
     in_order(root);
     printf("\n");
+
+    puts("Removing...");
+    remove_node(root, 99);
+    remove_node(root, 99);
 }
